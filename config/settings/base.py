6cb627629cfa75df5bb32fe62ac0ff92cae31835
Elena Williams <git@elena.net.au>
2018-02-08 19:26:55 +1100

    Fix url namespace on LOGIN_REDIRECT_URL in settings
    This is in addition to last commit though may cause trouble in
    deployment and is probably not the ultimate behaviour we want. Probably
    need to fix this later.

--- config/settings/base.py
+++ config/settings/base.py
@@ -266,7 +266,7 @@ SOCIALACCOUNT_ADAPTER = 'project.users.adapters.SocialAccountAdapter'
 # Custom user app defaults
 # Select the correct user model
 AUTH_USER_MODEL = 'users.User'
-LOGIN_REDIRECT_URL = 'users:redirect'
+LOGIN_REDIRECT_URL = 'user_redirect'
 LOGIN_URL = 'account_login'
 
 # SLUGLIFIER
