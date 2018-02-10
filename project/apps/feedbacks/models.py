d733d2d859e1699db3443e3d754329fe7585cfa2
Elena Williams <git@elena.net.au>
2018-02-10 23:23:03 +1100

    Add message email on save to Feedback

--- project/apps/feedbacks/models.py
+++ project/apps/feedbacks/models.py
@@ -1,3 +1,4 @@
+from django.core.mail import send_mail
 from django.db import models
 
 
@@ -16,3 +17,22 @@ class Feedback(models.Model):
 
     def __str__(self):
         return "{}: {} ({})".format(self.date_create, self.name, self.email)
+
+    def email_feedback(self):
+        message = """{}
+{}
+---
+message:
+{}
+""".format(self.name, self.email, self.message)
+        send_mail(
+            'Fifty50: Feedback received from website',
+            message,
+            'fifty50mentoring+webserver@gmail.com',
+            ['fifty50mentoring@gmail.com'],
+            fail_silently=True,
+        )
+
+    def save(self, *args, **kwargs):
+        super(Feedback, self).save(*args, **kwargs)
+        self.email_feedback()
