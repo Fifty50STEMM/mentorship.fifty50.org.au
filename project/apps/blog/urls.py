4d3c5d876ec98f81c0e552a9bfdf593cacb64703
Elena Williams <git@elena.net.au>
2018-02-15 10:02:54 +1100

    Refactor blog to using CBVs, add blog templates

--- project/apps/blog/urls.py
+++ project/apps/blog/urls.py
@@ -2,6 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
-    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+
+    url(r'^$',
+        views.PostListView.as_view(), name='post_list'),
+
+    url(r'^post/(?P<slug>[-\w]+)/$',
+        views.PostDetailView.as_view(), name='post_detail'),
 ]
