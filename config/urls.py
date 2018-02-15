844656b693066c5390f3014f992aa94ba0514d41
Elena Williams <git@elena.net.au>
2018-02-15 12:03:35 +1100

    Fix import bug on FAQ

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,9 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
 
+from project.apps.faqs.views import FaqView
+
+
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
     url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
